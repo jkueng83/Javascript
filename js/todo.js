51ab8c4efef007440abf4e6733a870142238bd69
jkueng83 <johannes.kueng@gmx.at>
2020-07-22 22:07:09 +0200

    Update ToDo App

--- js/todo.js
+++ js/todo.js
@@ -16,3 +16,33 @@ function addTask(){
     alert("Klick Task")
 
 }
+
+
+function addToListTest (){
+
+var obj = document.getElementById('list');
+ obj.onclick = function(e) {
+	if(!e) e = window.event;
+	var el = e.target || e.srcElement; 
+	var neu = document.createElement('li');
+	neu.innerHTML = 'dein Text';
+    el.parentNode.insertBefore(neu, el);
+}
+
+
+}
+
+function addToList(){
+    var x = document.createElement("LI");
+    var inputText = document.getElementById("newTask").value;
+    
+    var t = document.createTextNode(inputText);
+    
+    x.appendChild(t);
+    x.classList.add("alert");
+    x.classList.add("alert-secondary");
+    
+    document.getElementById("toDoList").appendChild(x);
+
+    document.getElementById("newTask").value = ""; // Eingabefeld wieder leeren
+}
