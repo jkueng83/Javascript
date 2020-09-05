7b303350041cbcda478f6a55267be767849d08f7
jkueng83 <johannes.kueng@gmx.at>
2020-09-05 11:25:25 +0200

    1. Version ToDo App

--- js/todo.js
+++ js/todo.js
@@ -1,56 +1,112 @@
-const tasks =[];
+const tasks = [];
+let task = {
+  description: "Hier ist ein Beispiel für eine Aufgabe",
+  isDone: true,
+};
 
-function addTask(){
+tasks.push(task);
 
-    const task1 = {
-        description: "lalelu",
-        status : "new"
+printTaskList();
+
+function addTask() {
+  const task1 = {
+    description: "lalelu",
+    isDone: false,
+  };
+  const task2 = {
+    description: "wandern",
+    isDone: false,
+  };
+
+  console.log("added Task");
+
+  tasks.push(task1);
+  tasks.push(task2);
+
+  console.log("number of tasks: " + tasks.length);
+
+  alert("Klick Task");
+}
+
+function addToListTest() {
+  var obj = document.getElementById("list");
+  obj.onclick = function (e) {
+    if (!e) e = window.event;
+    var el = e.target || e.srcElement;
+    var neu = document.createElement("li");
+    neu.innerHTML = "dein Text";
+    el.parentNode.insertBefore(neu, el);
+  };
+}
+
+function addTask() {
+  let newTaskString = document.getElementById("newTask").value;
+
+  let task = {
+    description: newTaskString,
+    isDone: false,
+  };
+
+  tasks.push(task);
+
+  // das war zum Testn:  alert(task.description + "+" + task.isDone);
+
+  deleteValueNewTask(); //document.getElementById("newTask").value = ""; // Eingabefeld wieder leeren
+
+  printTaskList();
+}
+
+function printTaskList() {
+  document.getElementById("toDoList").innerHTML = getTaskList();
+}
+
+function getTaskList() {
+  let html = "";
+  let index = 0;
+  tasks.forEach((element) => {
+    //  html += element.description + "  johi  ";
+
+    let checked = "";
+
+    if (element.isDone) {
+      checked = "checked";
     }
 
-   console.log("added Task")
+    html +=
+      "<li class='row alert alert-secondary'>" +
+      "<span class='col-md-10'> " +
+      element.description +
+      "</span> " +
+      " <div class='col-md_2'> " +
+      " <input type='checkbox' aria-label='Checkbox'" +
+      checked +
+      " data-index='" +
+      index +
+      "' " +
+      " onclick='markTask(this)'" +
+      " /> Done " +
+      " </div> " +
+      " </li> ";
 
-    tasks.push(task1);
+    index++;
+  });
 
-    console.log("number of tasks: " + tasks.length)
+  // zum testen: console.log(html);
 
-    alert("Klick Task")
-
+  return html;
 }
 
-
-function addToListTest (){
-
-var obj = document.getElementById('list');
- obj.onclick = function(e) {
-	if(!e) e = window.event;
-	var el = e.target || e.srcElement; 
-	var neu = document.createElement('li');
-	neu.innerHTML = 'dein Text';
-    el.parentNode.insertBefore(neu, el);
+function deleteValueNewTask() {
+  document.getElementById("newTask").value = ""; // Eingabefeld wieder leeren
 }
 
+function markTask(element) {
+  let index = element.attributes["data-index"].value;
+  let checked = element.checked;
 
-}
-
-function addToList(){
-    var liElement = document.createElement("LI");
-    var inputText = document.getElementById("newTask").value;
-    
-    var t = document.createTextNode(inputText);
-    
-    liElement.appendChild(t);
-    liElement.classList.add("row");
-    liElement.classList.add("alert");
-    liElement.classList.add("alert-secondary");
-    
-    document.getElementById("toDoList").appendChild(liElement);
-    
-    deleteValueNewTask(); //document.getElementById("newTask").value = ""; // Eingabefeld wieder leeren
-}
-
-
-
-
-function deleteValueNewTask (){
-    document.getElementById("newTask").value = ""; // Eingabefeld wieder leeren
+  if (checked) {
+    tasks[index].isDone = true;
+  } else {
+    tasks[index].isDone = false;
+  }
 }
